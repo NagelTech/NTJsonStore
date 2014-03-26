a42593f008bc1b824c3b328e5f9e59a454c6f516
Ethan Nagel <eanagel@gmail.com>
2014-03-25 22:26:08 -0700

    empty sample project

---
+++ NTJsonStoreSample/NTJsonStoreSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NTJsonStoreSample
+//
+//  Created by Ethan Nagel on 3/25/14.
+//  Copyright (c) 2014 NagelTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
